fdb2a8b1d0cd940faac5af6cf7730b167de04efa
Tribe <cindywidinda@gmail.com>
2022-11-12 22:09:17 +0700

    edit

--- tugas2.js
+++ tugas2.js
@@ -1,11 +1,19 @@
 function greetings (name){
-    //console.log(name)
     if(name === "cindy"){
         return "Hi!"
     }else{
         return "FAILED"
     }
 }
-const add = greetings('cindy') + ' welcome'
-
+const bio = {
+    firstName: "Welcome to ",
+    lastName: "my world!",
+    greetings: function(greets){
+        const a = this.firstName
+        const b = this.lastName
+        console.log(a+b + greets)
+    }
+}
+const add = greetings('cindy')
 console.log(add)
+bio.greetings(" Enjoy~")
